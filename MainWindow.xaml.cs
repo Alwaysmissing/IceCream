fc7b9d5261a785f403c09ef1db1c193442540099
Alwaysmissing <46465768+Alwaysmissing@users.noreply.github.com>
2024-07-05 10:01:30 +0800

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using IceCream.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IceCream
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainWindowViewModel vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if(vm == null)
+            {
+                vm = new MainWindowViewModel();
+                DataContext = vm;
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            vm?.machineState.SaveState();
+            base.OnClosed(e);
+        }
+    }
+}
